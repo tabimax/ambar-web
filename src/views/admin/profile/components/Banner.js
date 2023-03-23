144edb20f7d1b84864a875322d07489f6965f425
dinxsh <16219421127@atmiyauni.edu.in>
2023-03-23 22:53:09 +0530

    updat

--- src/views/admin/profile/components/Banner.js
+++ src/views/admin/profile/components/Banner.js
@@ -4,7 +4,7 @@ import Card from "components/card/Card.js";
 import React from "react";
 
 export default function Banner(props) {
-  const { banner, avatar, name, job, posts, followers, following } = props;
+  const { banner, avatar, name, job, badges, ambar, verified } = props;
   // Chakra Color Mode
   const textColorPrimary = useColorModeValue("secondaryGray.900", "white");
   const textColorSecondary = "gray.400";
@@ -38,27 +38,27 @@ export default function Banner(props) {
       </Text>
       <Flex w='max-content' mx='auto' mt='26px'>
         <Flex mx='auto' me='60px' align='center' direction='column'>
-          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-            {posts}
-          </Text>
           <Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-            Posts
+            Badges
+          </Text>
+          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
+            {badges}
           </Text>
         </Flex>
         <Flex mx='auto' me='60px' align='center' direction='column'>
-          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-            {followers}
-          </Text>
           <Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-            Followers
+            Ambar
+          </Text>
+          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
+            {ambar}
           </Text>
         </Flex>
         <Flex mx='auto' align='center' direction='column'>
-          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
-            {following}
-          </Text>
           <Text color={textColorSecondary} fontSize='sm' fontWeight='400'>
-            Following
+            Verified
+          </Text>
+          <Text color={textColorPrimary} fontSize='2xl' fontWeight='700'>
+            {verified}
           </Text>
         </Flex>
       </Flex>
