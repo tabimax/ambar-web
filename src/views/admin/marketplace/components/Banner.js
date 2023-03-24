5f9fded99b30c1599e947aac63dd0dff199b448b
dinxsh <16219421127@atmiyauni.edu.in>
2023-03-24 11:37:58 +0530

    removed unused vars

--- src/views/admin/marketplace/components/Banner.js
+++ src/views/admin/marketplace/components/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Chakra imports
-import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
 
 // Assets
 import banner from "assets/img/nfts/NftBanner1.png";
